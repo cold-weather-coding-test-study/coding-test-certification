adb94d982ad9dd4a932b1d97d3f4adfaf3f53ce4
JaeHunKang <dawin00k@gmail.com>
2025-03-23 20:39:28 +0900

    BJ_1699_제곱수의_합 문제풀이

--- JaeHunGang/3주차/BJ_1699_제곱수의_합.js
+++ JaeHunGang/3주차/BJ_1699_제곱수의_합.js
@@ -1,1 +1,17 @@
- 
+const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+
+// i는 항의 최소 개수를 구하려는 자연수
+// dp[i]는 현재 항의 최소 갯수
+// 경우는 i를 만들 수 있는 식
+// 식은 제곱수들의 합의 조합
+// 항의 갯수는 식의 제곱수 갯수
+const dp = Array(parseInt(input, 10) + 1).fill(Infinity);
+dp[0] = 0;
+
+for (let i = 1; i <= input; i++) {
+  for (let j = 1; j * j <= i; j++) {
+    dp[i] = Math.min(dp[i], dp[i - j * j] + 1); // if(i -j^2 > 0) i = i - j^2 + 1
+  }
+}
+
+console.log(dp[input]);
