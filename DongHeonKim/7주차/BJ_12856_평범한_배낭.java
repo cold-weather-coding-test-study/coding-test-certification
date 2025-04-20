78b8048abb766abd7b7394d69105681280fa986d
heondong0723 <heondong0723@gmail.com>
2025-04-20 10:21:51 +0900

    김동헌:7주차

---
+++ DongHeonKim/7주차/BJ_12856_평범한_배낭.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int[] dp = new int[K + 1];
+
+        for (int i = 0; i < N; i++) {
+            int weight = sc.nextInt();
+            int value = sc.nextInt();
+
+            for (int j = K; j >= weight; j--) {
+                dp[j] = Math.max(dp[j], dp[j - weight] + value);
+            }
+        }
+
+        System.out.println(dp[K]);
+    }
+}
